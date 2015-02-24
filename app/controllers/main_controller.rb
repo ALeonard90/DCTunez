8936a01ac296eccce4bd62b326e020fe5876148e
Andy Leonard <A.Leonard90@gmail.com>
2015-02-24 01:13:15 -0500

    spotify link with tfly headliner

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,10 +4,14 @@ class MainController < ApplicationController
 	@response = HTTParty.get("http://www.ticketfly.com/api/events/upcoming.json?orgId=355")
 	@headliner = @response["events"][0]["headlinersName"]
     @purchase = @response["events"][0]["ticketPurchaseUrl"]
-    artists = RSpotify::Artist.search(@headliner)
-    musician = artists.first
-    @toptracks = musician.top_tracks(:US)
+    @artists = RSpotify::Artist.search(@headliner)
+    @musician = @artists.first
+    @toptracks = @musician.top_tracks(:US)
+   	@openspot = @musician.external_urls['spotify']
+
 	end
 
 
 end
+
+
